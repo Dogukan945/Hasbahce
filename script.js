518d8c09cd5ba0f38f1b2daf2edbb44bdf04a945
Dogu <dogukan298@yandex.com>
2025-06-26 04:14:17 +0300

    s

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Hasbahçe site hazır');
+function showCategory(name) {
+    const content = document.getElementById('content');
+    content.innerHTML = `<h2>${name} kategorisi</h2><p>Ürünler buraya gelecek</p>`;
+}
+function openCart() {
+    const content = document.getElementById('content');
+    content.innerHTML = '<h2>Sepetiniz</h2><p>Henüz ürün eklenmedi.</p>';
+}
